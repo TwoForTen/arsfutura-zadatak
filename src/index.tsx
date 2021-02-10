73daca60660a1a6f13c79622793e18f1302c3c6c
Noel Rostohar <noel.rostohar@gmail.com>
2021-02-10 11:32:09 +0100

    Initial Redux Boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
+
+import reducers from './store';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
