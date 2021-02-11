e97de790c6f465f2da507b15afcb670eb1a0f2c4
Noel Rostohar <noel.rostohar@gmail.com>
2021-02-11 21:07:42 +0100

    Fetching Events

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import UserInfo from 'src/components/UserInfo/UserInfo';
+import { add } from 'date-fns';
+
+import { fetchEvents } from 'src/store/Events/actions';
 
 const Calendar: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEvents(add(new Date(), { days: 7 }).toISOString()));
+  }, []);
+
   return (
     <main>
       <UserInfo />
--- src/store/Events/actions.ts
+++ src/store/Events/actions.ts
@@ -1,14 +1,35 @@
 import { EventsActions, EventsState, EventsActionTypes } from './types';
+import { ThunkAction } from 'redux-thunk';
+import axios from 'src/axiosInstance';
 
-export const storeEvents = ({
-  summary,
-  start,
-  end,
-}: EventsState): EventsActions => {
+import { GlobalState } from '../';
+
+const storeEvents = ({ summary, start, end }: EventsState): EventsActions => {
   return {
     type: EventsActionTypes.STORE_EVENTS,
     summary,
     start,
     end,
+  };
+};
+
+export const fetchEvents = (
+  timeMax: string
+): ThunkAction<void, GlobalState, unknown, EventsActions> => {
+  return async (dispatch, getState) => {
+    const { access_token } = getState().user.token;
+
+    await axios
+      .get(
+        `/primary/events?timeMin=${new Date().toISOString()}&timeMax=${timeMax}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      )
+      .then(({ data }) => {
+        console.log(data);
+      });
   };
 };
