d29748a12c9ee123279e7f6c1411e0e2ff21d891
Noel Rostohar <noel.rostohar@gmail.com>
2021-02-12 22:20:05 +0100

    Event Card Design

---
+++ src/components/EventCard/EventCard.tsx
@@ -0,0 +1,40 @@
+import styles from './eventcard.module.scss';
+import { format } from 'date-fns';
+
+import { Event } from 'src/types';
+import deleteIcon from 'src/assets/delete.svg';
+import time from 'src/assets/time.svg';
+
+interface EventCardProps {
+  event: Event;
+}
+
+const EventCard: React.FC<EventCardProps> = ({ event }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.top_section}>
+        <h2 className={styles.summary}>{event.summary}</h2>
+        <button className={styles.button_delete}>
+          <img src={deleteIcon} alt="delete" />
+        </button>
+      </div>
+      <div className={styles.bottom_section}>
+        <div className={styles.time_container}>
+          <img src={time} alt="time" />
+          <div className={styles.event_title}>
+            <small>Start</small>
+            <span>{format(new Date(event.start), 'kk:mm')}</span>
+          </div>
+        </div>
+        <div className={styles.time_container}>
+          <div className={styles.event_title}>
+            <small>End</small>
+            <span>{format(new Date(event.end), 'kk:mm')}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,20 +1,32 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import UserInfo from 'src/components/UserInfo/UserInfo';
 import { add } from 'date-fns';
+import styles from './calendar.module.scss';
+
+import EventCard from 'src/components/EventCard/EventCard';
 
 import { fetchEvents } from 'src/store/Events/actions';
+import { GlobalState } from 'src/store';
+import { Event } from 'src/types';
 
 const Calendar: React.FC = () => {
   const dispatch = useDispatch();
+
+  const events = useSelector((state: GlobalState) => state.events);
 
   useEffect(() => {
     dispatch(fetchEvents(add(new Date(), { days: 7 }).toISOString()));
   }, [dispatch]);
 
   return (
-    <main>
+    <main className={styles.root}>
       <UserInfo />
+      <section className={styles.events_container}>
+        {events.map((event: Event) => {
+          return <EventCard key={event.summary} event={event} />;
+        })}
+      </section>
     </main>
   );
 };
