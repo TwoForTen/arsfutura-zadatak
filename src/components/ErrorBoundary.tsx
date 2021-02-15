bda0752112c70932f07a48252e9fb3a10746b003
Noel Rostohar <noel.rostohar@gmail.com>
2021-02-15 12:54:41 +0100

    Access Token Not Updating Bug Fix

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -8,40 +8,40 @@ import { GlobalState } from 'src/store';
 
 const ErrorBoundary: React.FC = ({ children }) => {
   const dispatch = useDispatch();
-  const { access_token } = useSelector(
-    (state: GlobalState) => state.user.token
-  );
-
-  const request = axios.interceptors.request.use((request) => {
-    request.headers = {
-      ...request.headers,
-      Authorization: `Bearer ${access_token}`,
-    };
-    return request;
-  });
-
-  const response = axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      switch (error.response.status) {
-        case 403:
-        case 401:
-          dispatch(logout());
-          break;
-        default:
-          break;
-      }
-    }
+  const access_token = useSelector(
+    (state: GlobalState) => state.user.token.access_token
   );
 
   useEffect(() => {
+    const request = axios.interceptors.request.use((request) => {
+      request.headers = {
+        ...request.headers,
+        Authorization: `Bearer ${access_token}`,
+      };
+      return request;
+    });
+
+    const response = axios.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        switch (error.response.status) {
+          case 403:
+          case 401:
+            dispatch(logout());
+            break;
+          default:
+            break;
+        }
+      }
+    );
+
     return () => {
       axios.interceptors.request.eject(request);
       axios.interceptors.response.eject(response);
     };
-  }, [response, request]);
+  }, [access_token, dispatch]);
 
   return <>{children}</>;
 };
